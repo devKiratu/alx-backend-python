6d4d47254609a0642fc3e973eba8fe7826279b96
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-07 14:56:36 +0300

    add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains a function for returning a whole number from a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the whole number part of a float n
+    """
+    return int(n)
