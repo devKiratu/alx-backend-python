eb530e2ae4974579edce2033e57991e76056ab22
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-07 15:01:29 +0300

    add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains a function for converting varible to strings"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string equivalent of a float n
+    """
+    return str(n)
