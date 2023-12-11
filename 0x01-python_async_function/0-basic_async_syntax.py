4bbe4c33bdad5f6f3ccd20dde24818a7a4f0e751
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-11 09:13:47 +0300

    type annotate functions

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay:int = 10) -> float:
     """Performs a non-blocking delay for a randomly generated wait period"""
     delay = random.random() * max_delay
     await asyncio.sleep(delay)
