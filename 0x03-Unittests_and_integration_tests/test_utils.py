aa668d03c1ad6201e2589f3a15e58c67db7c1431
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-04 16:30:45 +0300

    add solutions to task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,18 @@ class TestAccessNestedMap(unittest.TestCase):
         tests access_nested_map method behaves as expected
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        tests whether the method access_nested_map raises KeyError exceptions
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+if __name__ == '__main__':
+    unittest.main()
