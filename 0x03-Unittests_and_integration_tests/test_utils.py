fbcd19615751e0a7a65513556d05fa60a1b0abf0
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-05 14:01:23 +0300

    add solutions to task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,8 @@ Defines unit tests for the utils module
 from parameterized import parameterized
 import unittest
 from unittest.mock import Mock, patch
-from utils import access_nested_map, get_json
+import os
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -53,6 +54,36 @@ class TestGetJson(unittest.TestCase):
             response = get_json(test_url)
             self.assertDictEqual(response, test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    unit tests class for the utlis.memoize function/decorator
+    """
+    def test_memoize(self):
+        """
+        tests that utils.memoize works correctly
+        """
+        class TestClass:
+            """
+            test class that makes use of the memoize decorator
+            """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+            test_class = TestClass()
+            case_1 = test_class.a_property
+            case_2 = test_class.a_property
+
+            mock_a_method.assert_called_once()
+            self.assertEqual(case_1, 42)
+            self.assertEqual(case_2, 42)
 
 
 if __name__ == '__main__':
