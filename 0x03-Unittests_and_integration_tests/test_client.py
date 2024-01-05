828ec85c7b9145e92d596eb7f9f63bc0db0fa5f7
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-05 16:44:47 +0300

    add solution to task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 Defines unit tests for the client module
 """
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -27,6 +27,21 @@ class TestGithubOrgClient(unittest.TestCase):
             github_client.org()
 
             mock_get.assert_called_once_with(url)
+
+    @parameterized.expand([('google')])
+    def test_public_repos_url(self, org_name):
+        """
+        unit-test GithubOrgClient._public_repos_url method
+        """
+        with patch('utils.requests.get') as mock_get:
+            github_client = GithubOrgClient(org_name)
+            payload = {
+                'repos_url': 'https://api.github.com/orgs/google/repos'
+            }
+            mock_get.return_value.json.return_value = payload
+
+            self.assertEqual(github_client._public_repos_url,
+                             payload['repos_url'])
 
 
 if __name__ == "__main__":
